113e05e90c7439b6200b2c6c642304ea90c9d257
Yank32 <67502426+Yank32@users.noreply.github.com>
2020-07-12 18:26:07 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,9 +5,6 @@ const prefix = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
 
-     if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`vc nao possui permissao para ver os comandos dos staffs.`)
-
-
     if(args[0] == "help") return message.channel.send(`coloque ${prefix}help.`)
 
     if(args[0]) {
@@ -15,20 +12,20 @@ module.exports.run = async (client, message, args) => {
         if(client.commands.has(command)) {
             command = client.commands.cache.get(command);
             var SHembed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
+            .setColor("cyan")
             .setAuthor(`DualityMC`, message.guild.iconURL)
             .setThumbnail(client.user.displayAvatarURL)
-            .setDescription(`O Prefix do bot é: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n**Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
+            .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n**Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
             message.channel.send(SHembed);
         }}
 
     if(!args[0]) {
         message.delete();
-        
+
         let embed = new Discord.MessageEmbed()
-        .setAuthor(`🌀Comandos Da DualityMC!🌀`, message.guild.iconURL)
-        .setColor("RANDOM")
-        .setDescription(`${message.author.username} ***Mandei no seu privado <3!***`)
+        .setAuthor(`Comandos Rocket Host!`, message.guild.iconURL)
+        .setColor("#f94343")
+        .setDescription(`${message.author.username} ***Mandei no seu privado !***`)
 
         let Sembed = new Discord.MessageEmbed()
         .setColor("RANDOM")
@@ -36,17 +33,20 @@ module.exports.run = async (client, message, args) => {
         .setThumbnail(client.user.displayAvatarURL)
         .setTimestamp()
         .setDescription(`***Esses são meus comandos atuais!***\n**Meu prefix é: -**`)
-        .addField(`Comando#1:`, "``d!Ban <@usuario> <motivo>``")
-        .addField(`Comando#2:`, "``d!Kick <@usuario> <motivo>``")
-        .addField(`Comando#3:`, "``d!Limpar <1 a 99> (para limpar mensagens)``")
-        .addField(`Comando#4:`, "``d!Anuncio (siga os passos)``")
-        .addField(`Comando#5:`, "``d!Aviso <@usuario> <msg de aviso para o usuario>``")
-        .addField(`Comando#6:`, "``d!Dizer <para fazer o bot dizer algo``")
-        .addField(`Comando#7:`, "``d!Ping <para ver o ping do bot>``")
-        .addField(`Comando#8:`, "``d!User <para ver seus dados>``")
-        .addField(`Comando#9:`, "``d!Avatar <@usuario> (para ver a imagem de perfil de alguém)``")
-        .addField(`Comando#10:`, "``d!Uptime (para ver o tempo de atividade do bot)``")
-        .addField(`Comando#11:`, "``d!Info (para ver as informações do servidor)``")
+        .addField(`Comando#1:`, "``d!Avatar <@usuario> (Para ver a foto de alguem)``")
+        .addField(`Comando#2:`, "``d!Info (Para ver as informações do servidor)``")
+        .addField(`Comando#3:`, "``d!Jokempo (Jokempo com o Bot)``")
+        .addField(`Comando#4:`, "``d!Moeda (Cara ou coroa com o Bot)``")
+        .addField(`Comando#5:`, "``d!Uptime (Para ver o tempo on do bot)``")
+        .addField(`Comando#6:`, "``d!User (Para ver suas informações)``")
+        .addField(`Comando#7:`, "``d!Roleta (Roleta Russa kkk)``")
+        .addField(`Comando#8:`, "``d!Socar <@usuario> (Dar uma porrada em alguem)``")
+        .addField(`Comando#9:`, "``d!Palmas <@usuario> (Bater balmas para alguem)``")
+        .addField(`Comando#10:`, "``d!Pergunta <Sua pergunta> (Para fazer uma pergunta com resposta aleatoria do bot) ``")
+        .addField(`Comando#11:`, "``d!Kiss <@usuario> (Para beijar alguem)``")
+        .addField(`Comando#12:`, "``d!Casar <@usuario> (Para se casar com alguem)``")
+        .addField(`Comando#13:`, "``d!Divorcio <@usuario> (Para se divorciar de alguem)``")
+        .addField(`Comando#14:`, "``d!Reportar (Para reportar alguem)``")
         .setFooter("DualityMC", client.user.displayAvatarURL)
         message.author.send(Sembed)
     }
