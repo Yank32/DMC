901db08c9c6637db18c470252a2c8672d528c126
Yank32 <67502426+Yank32@users.noreply.github.com>
2020-07-12 18:29:10 -0300

    Update helpstaff.js

--- commands/helpstaff.js
+++ commands/helpstaff.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args) => {
             command = client.commands.cache.get(command);
             var SHembed = new Discord.MessageEmbed()
             .setColor("RANDOM")
-            .setAuthor(`Rocket Host`, message.guild.iconURL)
+            .setAuthor(`DualityMC`, message.guild.iconURL)
             .setThumbnail(client.user.displayAvatarURL)
             .setDescription(`O Prefix do bot é: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n**Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
             message.channel.send(SHembed);
@@ -36,11 +36,11 @@ module.exports.run = async (client, message, args) => {
         .setThumbnail(client.user.displayAvatarURL)
         .setTimestamp()
         .setDescription(`***Esses são meus comandos atuais!***\n**Meu prefix é: -**`)
-        .addField(`Comando#1:`, "``-Ban <@usuario> <motivo>``")
-        .addField(`Comando#2:`, "``-Kick <@usuario> <motivo>``")
-        .addField(`Comando#3:`, "``-Limpar <1 a 99> (para limpar mensagens)``")
-        .addField(`Comando#4:`, "``-Anuncio (siga os passos)``")
-        .addField(`Comando#5:`, "``-Aviso <@usuario> <msg de aviso para o usuario>``")
+        .addField(`Comando#1:`, "``d!Ban <@usuario> <motivo>``")
+        .addField(`Comando#2:`, "``d!Kick <@usuario> <motivo>``")
+        .addField(`Comando#3:`, "``d!Limpar <1 a 99> (para limpar mensagens)``")
+        .addField(`Comando#4:`, "``d!Anuncio (siga os passos)``")
+        .addField(`Comando#5:`, "``d!Aviso <@usuario> <msg de aviso para o usuario>``")
         .setFooter("DualityMC", client.user.displayAvatarURL)
         message.author.send(Sembed)
     }
